760cce40b2b7777880270d5eb21e84c2f0058b3e
(cdep)illabout <cdep.illabout@gmail.com>
2017-09-04 01:32:44 +0900

    Start adding in db stuff.

--- kitty-cat-map-db/src/Kitty/Db/Query.hs
+++ kitty-cat-map-db/src/Kitty/Db/Query.hs
@@ -1,10 +1,34 @@
 
 module Kitty.Db.Query where
 
-import Database.PostgreSQL.Simple
+import Control.Lens (view)
+import Control.Monad.Base (liftBase)
+import Data.Pool (withResource)
+import Database.PostgreSQL.Simple (Connection, FromRow, Query, ToRow, query, query_)
 
-hello :: IO Int
-hello = do
-  conn <- connectPostgreSQL ""
-  [Only i] <- query_ conn "select 2 + 2"
-  pure i
+import Kitty.Db.Conf (HasPool(pool))
+import Kitty.Db.Model (ImageInfo)
+
+-- hello :: IO Int
+-- hello = do
+--   conn <- connectPostgreSQL ""
+--   [Only i] <- query_ conn "select 2 + 2"
+--   pure i
+
+runDb :: (MonadBaseControl IO m, MonadReader r m, HasPool r) => (Connection -> m a) -> m a
+runDb f = do
+  pool' <- view pool
+  withResource pool' f
+
+dbGetImage :: (MonadBaseControl IO m, MonadReader r m, HasPool r) => m [ImageInfo]
+dbGetImage = runDb $ quer_ "SELECT * from image_info"
+
+quer
+  :: (FromRow r, MonadBase IO m, ToRow q)
+  => Query -> q -> Connection -> m [r]
+quer query' substitution conn = liftBase $ query conn query' substitution
+
+quer_
+  :: (FromRow r, MonadBase IO m)
+  => Query -> Connection -> m [r]
+quer_ query' conn = liftBase $ query_ conn query'
--- kitty-cat-map-server/src/Kitty/Server/Conf.hs
+++ kitty-cat-map-server/src/Kitty/Server/Conf.hs
@@ -10,8 +10,8 @@ import Kitty.Db
         Pool, mkDbConf)
 
 data ServerConf = ServerConf
-  { serverConfDbConf :: DbConf
-  , serverConfPort :: Port
+  { serverConfDbConf :: !DbConf
+  , serverConfPort :: {-# UNPACK #-} !Port
   }
 
 class HasServerConf s where
@@ -39,39 +39,6 @@ instance HasPort Port where
 instance HasPort ServerConf where
   port :: Lens' ServerConf Port
   port = lens serverConfPort (\s a -> s {serverConfPort = a})
-
--- -- | This 'Config' object is used to store environment about our application.
--- -- It is created on startup and passed to all the handlers.
--- data Conf = Conf
---   { {- confPool :: !ConnectionPool  -- ^ A pool of database connections.
---   , -} confPort :: !Port            -- ^ 'Port' to listen on.
---   }
-
--- -- | Number of simultaneous database connections to use in the
--- -- 'ConnectionPool'.
--- type DbPoolConnNum = Int
-
--- -- | Create a 'ConnectionPool' for database connections based on a
--- -- 'ConnectionString'.
--- makePoolFromUrl
---   :: DbPoolConnNum      -- ^ Number of database connections to use.
---   -> ConnectionString
---   -> IO ConnectionPool
--- makePoolFromUrl dbConnNum connectionString =
---   runStdoutLoggingT $ createPostgresqlPool connectionString dbConnNum
-
--- -- | Create a 'Config' based on environment variables, using defaults if the
--- -- environment variables don't exist.
--- createConfEnv :: IO Config
--- createConfigFromEnvVars = do
---   port <- readEnvVarDef "PORT" 8080
---   dbConnNum <- readEnvVarDef "DATABASE_CONNECTION_NUM" 10
---   dbConnectionString <-
---     lookupEnvDef
---       "DATABASE_URL"
---       "postgres://mydbuser:mydbpass@localhost:5432/mydb"
---   pool <- makePoolFromUrl dbConnNum dbConnectionString
---   pure Config {configPool = pool, configPort = port}
 
 mkServerConfEnv :: MonadIO m => m ServerConf
 mkServerConfEnv = do
