e34cb419946b936cefa84c606bbfee4ea8a88eeb
(cdep)illabout <cdep.illabout@gmail.com>
2017-09-09 14:34:58 +0900

    Adding insert statement for image.

--- kitty-cat-map-db/src/Kitty/Db/Query.hs
+++ kitty-cat-map-db/src/Kitty/Db/Query.hs
@@ -4,10 +4,18 @@ module Kitty.Db.Query where
 import Control.Lens (view)
 import Control.Monad.Base (liftBase)
 import Data.Pool (withResource)
-import Database.PostgreSQL.Simple (Connection, FromRow, Query, ToRow, query, query_)
+import Database.PostgreSQL.Simple
+       (Connection, FromRow, Query, ToRow, formatQuery, query, query_)
 
 import Kitty.Db.Conf (HasPool(pool))
-import Kitty.Db.Model (ImageInfo)
+import Kitty.Db.Model
+       (ImageInfo'(ImageInfo, imageFileName, imageGeom), ImageInfo,
+        ImageInfoData, ImageInfoKey)
+
+data QueryError = QueryError Text
+  deriving (Eq, Read, Show)
+
+instance Exception QueryError
 
 -- hello :: IO Int
 -- hello = do
@@ -21,7 +29,16 @@ runDb f = do
   withResource pool' f
 
 dbGetImages :: (MonadBaseControl IO m, MonadReader r m, HasPool r) => m [ImageInfo]
-dbGetImages = runDb $ quer_ "SELECT * from image_info"
+dbGetImages = runDb $ quer_ "SELECT * FROM image_info"
+
+dbCreateImage
+  :: (MonadBaseControl IO m, MonadReader r m, MonadThrow m, HasPool r)
+  => ImageInfoData -> m ImageInfoKey
+dbCreateImage ImageInfo {imageFileName, imageGeom} = do
+  runDb $
+    querS
+      "INSERT INTO image_info (filename, geom) values (?, ?) RETURNING id"
+      (imageFileName, imageGeom)
 
 quer
   :: (FromRow r, MonadBase IO m, ToRow q)
@@ -32,3 +49,24 @@ quer_
   :: (FromRow r, MonadBase IO m)
   => Query -> Connection -> m [r]
 quer_ query' conn = liftBase $ query_ conn query'
+
+querS
+  :: (FromRow r, MonadBase IO m, MonadThrow m, ToRow q)
+  => Query -> q -> Connection -> m r
+querS query' substitution conn = do
+  res' <- quer query' substitution conn
+  let fullQueryM = formatQuery conn query' substitution
+  case res' of
+    [] -> do
+      formatted <- liftBase fullQueryM
+      let msg =
+            "received no results from q query that should return only a " <>
+            "single result: " <> decodeUtf8 formatted
+      throwIO $ QueryError msg
+    [res] -> pure res
+    _ -> do
+      formatted <- liftBase fullQueryM
+      let msg =
+            "received multiple results from q query that should return only " <>
+            "a single result: " <> decodeUtf8 formatted
+      throwIO $ QueryError msg
